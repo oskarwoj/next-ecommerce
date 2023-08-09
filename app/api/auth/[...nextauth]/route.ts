ea12032a615f3c3c12ba7af11e2bafce59a97395
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-09 15:54:05 +0200

    Added Stripe Configuration

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,44 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import Stripe from "stripe";
+
+const prisma = new PrismaClient();
+
+const handler = NextAuth({
+  //@ts-ignore
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  //Add another provider
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15",
+      });
+      // Let's create a stripe customer
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        });
+        // Update our prisma user with the stripeCustomerId
+        await prisma.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            stripeCustomerId: customer.id,
+          },
+        });
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
