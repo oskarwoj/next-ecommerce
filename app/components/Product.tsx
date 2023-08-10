169d0ed49adb54e660fd82b2c1dd4d42cd1e3fff
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-10 15:15:48 +0200

    Created additional styles

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -16,11 +16,15 @@ const Product: React.FC<ProductProps> = ({ name, image, price }) => {
         width={800}
         height={800}
         alt={name}
-        className="w-full h-80 object-cover cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out rounded-md"
+        className="w-full h-80 object-cover cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out rounded-lg"
         priority={true}
       />
-      <h1 className="text-sm font-semibold pt-4">{name}</h1>
-      <p>{formatPrice(price)}</p>
+      <h1 className="text-sm font-medium text-gray-700 mt-2 m-y-2 py-2 border-b-2 border-gray-200 border-solid">
+        {name}
+      </h1>
+      <p className="text-lg font-semibold text-gray-700">
+        {formatPrice(price)}
+      </p>
     </div>
   );
 };
