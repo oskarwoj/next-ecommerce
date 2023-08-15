0f0141ef1af922b69938e2dcf3196de8e869c001
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-15 18:26:07 +0200

    Created Payment Status Webhook

---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,54 @@
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+import { prismadb } from "@/util/prismadb";
+import { stripe } from "@/util/stripe";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
+
+  if (!signature) {
+    return new NextResponse(`Missing the stripe signature`, { status: 400 });
+  }
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+  }
+
+  switch (event.type) {
+    case "payment_intent.created":
+      const paymentIntent = event.data.object;
+      console.log("Payment intent was created", paymentIntent);
+      break;
+    case "charge.succeeded":
+      const charge = event.data.object as Stripe.Charge;
+      if (typeof charge.payment_intent === "string") {
+        await prismadb.order.update({
+          where: { paymentIntentID: charge.payment_intent },
+          data: { status: "complete" },
+        });
+      }
+      break;
+    default:
+      console.log("Unhandled event type:" + event.type);
+
+      return new NextResponse(null, { status: 200 });
+  }
+  return NextResponse.json({ received: true });
+}
